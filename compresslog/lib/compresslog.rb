fa4ad7cb87b84b028c856321c6854d1b957bed89
sbvorpagel <samuel.vorpagel@alfacon.com.br>
2014-10-16 09:53:50 -0300

    retirado o module

--- compresslog/lib/compresslog.rb
+++ compresslog/lib/compresslog.rb
@@ -1,57 +1,53 @@
 require 'zip'
 
-module Compresslog
+def backutp_is_needed?
+  dir = Dir.entries("#{Rails.root.join("log")}").grep /^#{Rails.env}.log./
+  return !dir.empty?
+end
 
-  def backutp_is_needed?
-    dir = Dir.entries("#{Rails.root.join("log")}").grep /^#{Rails.env}.log./
-    return !dir.empty?
+def log_compress
+  data = Time.new.strftime("%Y%m%d_%H%M%S")
+  folder = Rails.root.join("log")
+  input_filenames = Dir.entries("#{folder}").grep /^#{Rails.env}.log./
+  zipfile_name = "#{folder}/#{data}.zip"
+
+  Zip::File.open(zipfile_name, Zip::File::CREATE) do |zipfile|
+    input_filenames.each do |filename|
+      zipfile.add(filename, "#{folder}/#{filename}")
+    end
+    zipfile.get_output_stream("README") { |os| os.write "O backup, dos logs, foi realizado com sucesso em: #{Time.now}" }
   end
+end
 
-  def log_compress
-    data = Time.new.strftime("%Y%m%d_%H%M%S")
-    folder = Rails.root.join("log")
-    input_filenames = Dir.entries("#{folder}").grep /^#{Rails.env}.log./
-    zipfile_name = "#{folder}/#{data}.zip"
+def log_delete
 
+  data = Time.new.strftime("%Y%m%d_%H%M%S")
+  folder = Rails.root.join("log")
+  input_filenames = Dir.entries("#{folder}").grep /^#{Rails.env}.log./
+  zipfile_name = "#{folder}/#{data}.zip"
+
+  input_filenames.each do |i|
+    File.delete("#{folder}/#{i}")
+  end
+end
+
+def log_compress_full
+  data = Time.new.strftime("%Y%m%d_%H%M%S")
+  folder = Rails.root.join("log")
+  input_filenames = Dir.entries("#{folder}").grep /^#{Rails.env}.log./
+  zipfile_name = "#{folder}/#{data}.zip"
+  unless (input_filenames.empty?)
     Zip::File.open(zipfile_name, Zip::File::CREATE) do |zipfile|
       input_filenames.each do |filename|
         zipfile.add(filename, "#{folder}/#{filename}")
       end
       zipfile.get_output_stream("README") { |os| os.write "O backup, dos logs, foi realizado com sucesso em: #{Time.now}" }
     end
-  end
-
-  def log_delete
-
-    data = Time.new.strftime("%Y%m%d_%H%M%S")
-    folder = Rails.root.join("log")
-    input_filenames = Dir.entries("#{folder}").grep /^#{Rails.env}.log./
-    zipfile_name = "#{folder}/#{data}.zip"
 
     input_filenames.each do |i|
       File.delete("#{folder}/#{i}")
     end
+  else
+    "backup desnecessário"
   end
-
-  def log_compress_full
-    data = Time.new.strftime("%Y%m%d_%H%M%S")
-    folder = Rails.root.join("log")
-    input_filenames = Dir.entries("#{folder}").grep /^#{Rails.env}.log./
-    zipfile_name = "#{folder}/#{data}.zip"
-    unless (input_filenames.empty?)
-      Zip::File.open(zipfile_name, Zip::File::CREATE) do |zipfile|
-        input_filenames.each do |filename|
-          zipfile.add(filename, "#{folder}/#{filename}")
-        end
-        zipfile.get_output_stream("README") { |os| os.write "O backup, dos logs, foi realizado com sucesso em: #{Time.now}" }
-      end
-
-      input_filenames.each do |i|
-        File.delete("#{folder}/#{i}")
-      end
-    else
-      "backup desnecessário"
-    end
-  end
-
 end
